def9ad1b57010f49e7af7ffbf19aa65a6c65b750
mitadrub <mitadrubanik@gmail.com>
2022-08-28 21:26:26 +0530

    added controller, service layer and updated the repository

--- src/main/java/com/department/departmentservice/repository/DepartmentRepository.java
+++ src/main/java/com/department/departmentservice/repository/DepartmentRepository.java
@@ -1,4 +1,9 @@
 package com.department.departmentservice.repository;
 
-public class DepartmentRepository {
+import com.department.departmentservice.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
 }
---
+++ src/main/java/com/department/departmentservice/service/DepartmentService.java
@@ -0,0 +1,16 @@
+package com.department.departmentservice.service;
+
+import com.department.departmentservice.entity.Department;
+import com.department.departmentservice.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartmentService {
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public Department saveDepartment(Department department) {
+        return departmentRepository.save(department);
+    }
+}
