a1d8073387ab70a377b580f31145cb2074f8f037
mitadrub <mitadrubanik@gmail.com>
2022-08-29 00:29:09 +0530

    added server port no in yml file and update on other layers

--- src/main/java/com/department/departmentservice/controller/DepartmentController.java
+++ src/main/java/com/department/departmentservice/controller/DepartmentController.java
@@ -2,20 +2,27 @@ package com.department.departmentservice.controller;
 
 import com.department.departmentservice.entity.Department;
 import com.department.departmentservice.service.DepartmentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/departments")
+@Slf4j
 public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
     @PostMapping("/")
     public Department saveDepartment(@RequestBody Department department) {
+        log.info("Inside saveDepartment method of DepartmentController");
         return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/{id}")
+    public Department findDepartmentById(@PathVariable("id") Long departmentId) {
+        log.info("Inside findDepartmentById method of DepartmentController");
+        return departmentService.findDepartmentById(departmentId);
+
     }
 }
